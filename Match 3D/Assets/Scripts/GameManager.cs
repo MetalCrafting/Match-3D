610b03c6f93a62f57cf24e0807d230df05249041
MetalCrafting <ninja.nightblack.micro.box@gmail.com>
2024-02-14 19:40:55 +0100

    Settings Update
    Added Settings Menu

--- Match 3D/Assets/Scripts/GameManager.cs
+++ Match 3D/Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
     private Transform FirstTrans = null;
     private GameObject SecondGameObj = null;
     private Transform SecondTrans = null;
-    [SerializeField] float CubeSpeed = 1f;
+    [SerializeField] float CubeSpeed = 10f;
     
     // Start is called before the first frame update
     void Start()
@@ -23,9 +23,8 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 100f))
+            if (Physics.Raycast(ray, out RaycastHit hit, 100f))
             {
                 if (hit.collider != null)
                 {
@@ -61,25 +60,53 @@ public class GameManager : MonoBehaviour
 
     public void Play4x4x4()
     {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ToSettings()
+    {
         SceneManager.LoadScene(1);
+    }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 
     public void SwapCubes(GameObject Cube1, GameObject Cube2, Transform Pos1, Transform Pos2)
     {
         foreach (Transform spawner in Field)
         {
-            spawner.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            if (spawner.gameObject.CompareTag("Spawner"))
+            {
+                spawner.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                spawner.gameObject.GetComponent<Collider>().enabled = false;
+            }
         }
 
         while (Cube1.transform.position != Pos2.transform.position)
         {
-            Cube1.transform.Translate(Pos2.transform.position * Time.deltaTime * CubeSpeed);
-            Cube2.transform.Translate(Pos1.transform.position * Time.deltaTime * CubeSpeed);
+            // Cube1.transform.Translate((Pos2.transform.position - Pos1.transform.position) * Time.deltaTime * CubeSpeed);
+            // Cube2.transform.Translate((Pos1.transform.position - Pos2.transform.position) * Time.deltaTime * CubeSpeed);
+            Cube1.transform.position = Pos2.transform.position;
+            Cube2.transform.position = Pos1.transform.position;
         }
 
         foreach (Transform spawner in Field)
         {
-            spawner.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            if (spawner.gameObject.CompareTag("Spawner"))
+            {
+                spawner.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+                spawner.gameObject.GetComponent<Collider>().enabled = true;
+            }
+        }
+
+        foreach (Transform cube in Field)
+        {
+            if (cube.TryGetComponent<CubeDestroyer>(out CubeDestroyer CubeDest))
+            {
+                CubeDest.DestroyCubes();
+            }
         }
     }
 }
